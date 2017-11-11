4b700b088e1aa529b7b0383e0e77fe812e747549
Marek Dziewit <marek2901@gmail.com>
2017-11-11 23:18:04 +0100

    Add API for Dishes (#5)
    * Add gignore for backend
    
    * Add api for recepies

---
+++ backend/lib/csv_to_json.py
@@ -0,0 +1,7 @@
+import csv
+
+def convert(filepath):
+    with open(filepath) as f:
+        a = [{k: v for k, v in row.items()}
+            for row in csv.DictReader(f, skipinitialspace=True)]
+        return a
