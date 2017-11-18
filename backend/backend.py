1ca63b785351b02b44b5116222be18455d954abc
Marek Dziewit <marek2901@gmail.com>
2017-11-18 23:37:44 +0100

    #FM14 Format Dishes

--- backend/backend.py
+++ backend/backend.py
@@ -3,16 +3,23 @@ import os
 import lib.csv_to_json
 import json
 
+import services.parse_dishes_service as parse_dishes_service
+
 app = Flask(__name__)
 
 
 @app.route('/api/v1/dishes')
-def hello_world():
+def get_dishes():
     data_path = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         'data',
         'recipes.csv')
-    return json.dumps(lib.csv_to_json.convert(data_path), ensure_ascii=False)
+
+    response = parse_dishes_service.call(
+        lib.csv_to_json.convert(data_path)
+    )
+
+    return json.dumps(response, ensure_ascii=False)
 
 
 if __name__ == '__main__':
